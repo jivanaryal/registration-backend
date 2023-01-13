612df9f1ed7d064b6eb5f026eeb07b9a9ff920f5
jivan aryal <jeevanaryal7947@gmail.com>
2023-01-13 16:42:22 +0545

    hello

--- middleware/error-handler.js
+++
@@ -1,5 +0,0 @@
-const errorHandlerMiddleware = (err, req, res, next) => {
-  return res.status(500).json({ msg: "something went wrong, try again later" });
-};
-
-module.exports = errorHandlerMiddleware;
--- middleware/not-found.js
+++
@@ -1,5 +0,0 @@
-const notFound = (req, res) => {
-  res.status(404).send("route doesnot exist");
-};
-
-module.exports = notFound;
