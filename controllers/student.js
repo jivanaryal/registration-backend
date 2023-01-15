387db734f31fdce5390b4c725f4c43e64fd0dbfd
jivan aryal <jeevanaryal7947@gmail.com>
2023-01-15 22:07:14 +0545

    push

--- controllers/student.js
+++ controllers/student.js
@@ -8,14 +8,11 @@ const getAllStudent = asyncWrapper(async (req, res) => {
 
 const createStudent = asyncWrapper(async (req, res) => {
   const student = await Student.create(req.body);
-  console.log(req.body);
+
   res.status(200).json(student);
 });
 
 const getSingleStudent = asyncWrapper(async (req, res) => {
-  // const { id } = req.params;
-  // console.log({ id });
-
   const student = await Student.findOne({ _id: req.params.id });
   if (!student) {
     res.status(500).json({ msg: `cannot get the id:${id}` });
@@ -33,10 +30,9 @@ const removeStudent = asyncWrapper(async (req, res) => {
 const updateStudent = asyncWrapper(async (req, res) => {
   const student = await Student.updateOne({ _id: req.params.id }, req.body);
   if (!student) {
-    res.statu;
-    res.status(200).json(student);
-    s(500).json({ msg: `cannot get the id:${id}` });
+    res.status(500).json({ msg: `cannot get the id:${id}` });
   }
+  res.status(200).json(student);
 });
 
 module.exports = {
