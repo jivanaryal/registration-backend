ee75362f1117e556d08fa4eeb46870d978193b61
jivan aryal <jeevanaryal7947@gmail.com>
2023-01-17 16:32:12 +0545

    hello

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`server is listing to the port ${port}`));
   } catch (error) {
     console.log(error);
