017c8ce5b5963e4787c4c32fce5004f397d441c6
jivan aryal <jeevanaryal7947@gmail.com>
2023-01-17 21:26:25 +0545

    hello

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const connectDB = require("./db/connect");
-require("dotenv").config();
+const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const student = require("./routes/student");
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+const morgan = require("morgan");
 const app = express();
-
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 app.use("/api/v1/student", student);
 app.use(notFound);
@@ -19,6 +22,7 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 const start = async () => {
   try {
+    // console.log(process.env.MONGO_URI);
     await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`server is listing to the port ${port}`));
   } catch (error) {
