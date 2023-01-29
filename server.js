ae5b7e3c4200fdd562965520e39f4d3edea52a66
jivan aryal <jeevanaryal7947@gmail.com>
2023-01-29 21:41:53 +0545

    push

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 dotenv.config();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://rm-system.netlify.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
