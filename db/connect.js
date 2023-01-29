c7fa99edb44d3d24a24865cb4839df9e53f693ba
jivan aryal <jeevanaryal7947@gmail.com>
2023-01-29 15:33:21 +0545

    hello

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async (url) => {
   mongoose.set("strictQuery", false);
   try {
-    await mongoose.connect(url);
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   } catch (error) {
     console.log(error);
   }
